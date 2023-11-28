ead34a1c587ed7590859ba9b2f009d9e7af8cfd3
mmenault <maxime.menault@orange.fr>
2023-11-28 23:27:47 +0100

    Last fix for JDR

--- Cailean/Irori/urls.py
+++ Cailean/Irori/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('Login', views.LoginView.as_view()),
     path('Logout', views.LogoutView.as_view()),
     path('Fiches/<int:version>',login_required(views.Fiches.as_view())),
-    path('Fiche/<int:version>/<int:perso_id>/<str:onglet>',login_required(views.FichePerso.as_view())),
     path('Fiche/<int:version>/<int:perso_id>/Lancer/<int:taille_des>/<int:carac_points>/<int:carac_id>/<int:comp_id>/<int:pouvoir_id>',login_required(views.lancerDes.as_view())),
     path('Fiche/<int:version>/<int:perso_id>/Attaque/<int:equipement_id>',login_required(views.Attaquer.as_view())),
     path('Fiche/<int:version>/<int:perso_id>/Update/<int:equipement_id>',login_required(views.updateEquipement.as_view())),
@@ -29,5 +28,6 @@ urlpatterns = [
     path('Village/<int:version>/<int:village_id>/Create',login_required(views.createEquipementVillage.as_view())),
     path('StatsDes/<int:version>',login_required(views.statsDes.as_view())),
     path('StatsDes/Get',login_required(views.statsDesGet.as_view())),
-    path('Session/<int:version>',login_required(views.SessionView.as_view()))
+    path('Session/<int:version>',login_required(views.SessionView.as_view())),
+    path('Fiche/<int:version>/<int:perso_id>/<str:onglet>',login_required(views.FichePerso.as_view()))
 ]
